85ea78c8295735fce4b0df41c433ec9bde3ce9eb
MarcosGutierrezMoya <marcos.gutierrez.moya2010@gmail.com>
2023-05-14 18:12:05 +0200

    Make a filter and trying to do a json-server

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,16 +1,22 @@
 import { Component, Input, inject } from '@angular/core';
 import { Location } from '@angular/common';
-// import { HttpClient } from '@angular/common/http';
-// import { Categoria } from '../interface/anime.Interface';
+import { HttpClient } from '@angular/common/http';
+// import { Anime } from '../interface/anime.Interface';
 import animesDb from '../../server/animesDb.json';
+import { Target } from '@angular/compiler';
 
-interface Categoria {
+interface Anime {
   id: number;
   title: string;
   numCap: number;
-  descripcion: string;
-  imagen: string;
+  description: string[];
+  favourite:Boolean;
+  img: string;
   link: string;
+  tags: String[];
+}
+interface Animes {
+  animesDB: {};
 }
 
 @Component({
@@ -20,24 +26,40 @@ interface Categoria {
 })
 export class PagesComponent {
   @Input() ruta = "";
-  // http = inject(HttpClient);
-  categoria: Categoria[] = [];
-
+  http = inject(HttpClient);
+  anime: Anime[] = [];
+  animesDB: {} = {};
+  showAll: Boolean = true;
+  favourite: Boolean = true;
 
   constructor(location: Location) {
 
   }
-
   ngOnInit() {
-    // this.http.get<Categoria[]>('../../server/animesDb.json')
-    //   .subscribe((data) => {
-    //     this.categoria = data;
-    //     console.log(data);
-    //   });
-    this.ruta = location.pathname.slice(1);
-    type StatusKey = keyof typeof animesDb;
-    let str = this.ruta as StatusKey;
-
-    this.categoria = animesDb[str];
+    this.http.get<Animes[]>('http://localhost:5000/animes')
+      .subscribe((data) => {
+        this.animesDB = data;
+      });
+      this.ruta = location.pathname.slice(1);
+      type StatusKey = keyof typeof animesDb;
+      let str = this.ruta as StatusKey;
+          
+      if (this.showAll) {
+        console.log(this.animesDB);
+        this.animesDB;
+        this.anime = animesDb.filter(anime => anime.tags.includes(str.toString()));
+      } else {
+        this.anime = animesDb.filter(anime=>anime.tags[0]===str);      
+      }
+      this.anime.sort(function (a, b) {
+      if (a.title < b.title) { return -1; }
+      if (a.title > b.title) { return 1; }
+      return 0;
+      })
   }
+
+  MakeFavourite(anime:any) {
+    anime.favourite = !anime.favourite;
+  }
+
 }
